bce3e5a63e9b4f900380a15460f006d7d12eabfb
sroopsai <roopsai.work@gmail.com>
2020-07-24 12:40:19 +0200

    Initial Commit

---
+++ src/org/roopsai/javatutorial/classesandobjects/Bicycle.java
@@ -0,0 +1,40 @@
+package org.roopsai.javatutorial.classesandobjects;
+
+public class Bicycle {
+
+    // the Bicycle class has
+    // three fields
+    public int gear;
+    public int speed;
+    public int cadence;
+
+    // the Bicycle class has one constructor
+    public Bicycle(int cadence, int gear, int speed)
+    {
+        this.cadence = cadence;
+        this.gear = gear;
+        this.speed = speed;
+    }
+
+    // the Bicycle class has four methods
+    public void setCadence(int newCadence)
+    {
+        cadence = newCadence;
+    }
+
+    public void setGear(int newGear)
+    {
+        gear = newGear;
+    }
+
+    public void applyBrake(int decrement)
+    {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment)
+    {
+        speed += increment;
+    }
+
+}
---
+++ src/org/roopsai/javatutorial/classesandobjects/MountainBike.java
@@ -0,0 +1,24 @@
+package org.roopsai.javatutorial.classesandobjects;
+
+public class MountainBike extends Bicycle {
+
+    // the MountainBike subclass has
+    // one field
+
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(int seatHeight, int cadence, int gear, int speed)
+    {
+        super(cadence, gear, speed);
+        this.seatHeight = seatHeight;
+    }
+
+    // the MountainBike subclass has
+    // one method
+
+    public void setSeatHeight(int newSeatHeight)
+    {
+        seatHeight = newSeatHeight;
+    }
+}
